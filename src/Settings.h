899d315d5fe64f24fa08e75a6fbd05e544e22c3e
powerof3 <32599957+powerof3@users.noreply.github.com>
2023-09-18 14:54:42 +0530

    Add option to change item weights

--- src/Settings.h
+++ src/Settings.h
@@ -12,11 +12,13 @@ public:
 
 		ini.LoadFile(path.c_str());
 
-		ini::get_value(ini, baseGravity, "Settings", "fGravityBase", nullptr);
+		ini::get_value(ini, baseGravity, "Settings", "fGravityBase", ";Base gravity scale.");
+		ini::get_value(ini, adjustItemWeights, "Settings", "bAdjustItemWeights", ";Gravity affects item weight. Items will weigh less in lower gravity.\n;If false, gravity will affect carry weight. You will have more carry weight in lower gravity");
 
 		(void)ini.SaveFile(path.c_str());
 	}
 
 	// members
 	float baseGravity{ 1.0 };
+	bool  adjustItemWeights{ true };
 };
--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,12 @@ namespace Handler
 		{
 			auto carryWeight = a_actor->GetActorValue(*RE::ActorValue::GetSingleton()->carryWeight);
 			if (a_useGravity) {
-                auto localGravity = a_actor->GetGravityScale();
+				auto localGravity = a_actor->GetGravityScale();
 				if (localGravity < 0.1f) {
 					localGravity = 0.1f;
 				}
 
+				// higher grav = less carry weight, lower grav = more carry weight
 				carryWeight *= Settings::GetSingleton()->baseGravity / localGravity;
 			}
 			RE::BGSEntryPoint::HandleEntryPoint(RE::BGSEntryPoint::ENTRY_POINT::kGetMaxCarryWeight, a_actor, std::addressof(carryWeight));
@@ -21,12 +22,39 @@ namespace Handler
 		static inline constexpr std::size_t size{ 0xBA };
 	};
 
+	struct AdjustItemWeight
+	{
+		static float Thunk(RE::Actor* a_this, std::uintptr_t a_inventoryStack, float a_itemWeight)
+		{
+			if (a_itemWeight > 0.0f) {
+				auto localGravity = a_this->GetGravityScale();
+				if (localGravity < 0.1f) {
+					localGravity = 0.1f;
+				}
+
+				// higher grav = weigh more, lower grav = weight less
+			    float itemWeight = a_itemWeight * localGravity / Settings::GetSingleton()->baseGravity;
+
+			    return func(a_this, a_inventoryStack, itemWeight);
+			}
+
+			return func(a_this, a_inventoryStack, a_itemWeight);
+		}
+		static inline REL::Relocation<decltype(Thunk)> func;
+		static inline std::size_t                      idx = 0x99;
+	};
+
 	void Install()
 	{
-		Settings::GetSingleton()->LoadSettings();
+		const auto settings = Settings::GetSingleton();
+		settings->LoadSettings();
 
-	    REL::Relocation<std::uintptr_t> target{ REL::Offset(0x024877CC) };
-		stl::asm_replace<GetMaxCarryWeight>(target.address());
+		if (settings->adjustItemWeights) {
+			stl::write_vfunc<AdjustItemWeight>(RE::VTABLE::PlayerCharacter[25]);
+		} else {
+			REL::Relocation<std::uintptr_t> target{ REL::Offset(0x024877CC) };
+			stl::asm_replace<GetMaxCarryWeight>(target.address());
+		}
 	}
 }
 
